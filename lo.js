5fc23c8c56c4ce43c046d3f09eef3825511dc273
witspp <97096846+witspp@users.noreply.github.com>
2022-01-04 17:34:48 +0530

    dn
    hhj

---
+++ lo.js
@@ -0,0 +1,34 @@
+n=-114
+function oddeven()
+
+{
+if( n%2==0){
+    if(n>0){
+
+        console.log("its even and +");
+
+    }
+    else{
++
+
+        console.log("its even but -");
+
+    }
+  
+    
+
+}
+else{
+    if(n<0)
+{
+    console.log("its odd and -");
+
+}    
+else{
+
+    console.log("its odd but +");
+}
+}
+
+}
+oddeven()
